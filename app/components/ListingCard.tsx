f2ec838148f5d127e58a6fef7591939ac6cc5cc2
SafaMaqbool <s.maq.basha@gmail.com>
2024-11-20 01:23:53 +0500

    Favorites page done (db issue)

--- app/components/ListingCard.tsx
+++ app/components/ListingCard.tsx
@@ -1,41 +1,79 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useCountries } from "../lib/getCountries";
+import { Heart } from "lucide-react";
+import {
+  AddToFavoritesButton,
+  DeleteFromFavoriteButton,
+} from "./SubmitButtons";
+import { AddToFavorite } from "../actions";
 
-interface iAppProps{
-    imagePath: string,
-    description: string,
-    location: string,
-    price: number,
-
+interface iAppProps {
+  imagePath: string;
+  description: string;
+  location: string;
+  price: number;
+  userId: string | undefined;
+  isInFavoritesList: boolean;
+  FavoriteId: string;
+  homeId: string;
+  pathName: string;
 }
 
-export function ListingCard({description,imagePath,location,price}:iAppProps){
-    const {getCountryByValue} = useCountries()
-    const country=getCountryByValue(location)
-    return (
-      <div className="flex flex-col">
-        <div className="relative h-72">
-          <Image
-            src={`https://jfcwufywhjfvdlouacmu.supabase.co/storage/v1/object/public/images/${imagePath}`}
-            alt="Image of House"
-            fill
-            className="rounded-lg h-full object-cover"
-          />
-        </div>
-
-        <Link className="mt-2" href={"/"}>
-          <h3 className="font-medium text-base">
-            {country?.flag} {country?.label}/{country?.region}
-          </h3>
-          <p className="text-muted-foreground text-sm line-clamp-2 ">
-            {description}
-          </p>
-          <p className="text-muted-foreground pt-2">
-            <span className="font-medium text-black">${price}</span>Night
-          </p>
-        </Link>
+export function ListingCard({
+  description,
+  imagePath,
+  location,
+  price,
+  userId,
+  FavoriteId,
+  homeId,
+  isInFavoritesList,
+  pathName,
+}: iAppProps) {
+  const { getCountryByValue } = useCountries();
+  const country = getCountryByValue(location);
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-72">
+        <Image
+          src={`https://jfcwufywhjfvdlouacmu.supabase.co/storage/v1/object/public/images/${imagePath}`}
+          alt="Image of House"
+          fill
+          className="rounded-lg h-full object-cover"
+        />
+        {userId && (
+          <div className="z-10 absolute top-2 right-2">
+            {isInFavoritesList ? (
+              <form action={DeleteFromFavoriteButton}>
+                <input type="hidden" name="favoriteId" value={FavoriteId} />
+                <input type="hidden" name="userId" value={userId} />
+                <input type="hidden" name="pathName" value={pathName} />
+                <DeleteFromFavoriteButton />
+              </form>
+            ) : (
+              <form action={AddToFavorite}>
+                <input type="hidden" name="homeId" value={homeId} />
+                <input type="hidden" name="userId" value={userId} />
+                <input type="hidden" name="pathName" value={pathName} />
+                <AddToFavoritesButton />
+              </form>
+            )}
+          </div>
+        )}
       </div>
-    );
-    
+
+      <Link className="mt-2" href={"/"}>
+        <h3 className="font-medium text-base">
+          {country?.flag} {country?.label}/{country?.region}
+        </h3>
+        <p className="text-muted-foreground text-sm line-clamp-2 ">
+          {description}
+        </p>
+        <p className="text-muted-foreground pt-2">
+          <span className="font-medium text-black">${price}</span>Night
+        </p>
+      </Link>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,12 @@ import { ListingCard } from "./components/ListingCard";
 import { Suspense } from "react";
 import { SkeletonCard } from "./components/SkeletonCard";
 import { Noitems } from "./components/Noitems";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 async function getData({
   searchParams,
+  userId,
 }: {
+  userId: string | undefined;
   searchParams?: { filter?: string };
 }) {
   const data = await prisma.home.findMany({
@@ -16,7 +19,7 @@ async function getData({
       addedCategory: true,
       addedLocation: true,
       addedDescription: true,
-      categoryName: searchParams?.filter ?? undefined
+      categoryName: searchParams?.filter ?? undefined,
     },
     select: {
       photo: true,
@@ -24,18 +27,25 @@ async function getData({
       price: true,
       description: true,
       country: true,
+      Favorite: {
+        where: {
+          userId: userId ?? undefined,
+        },
+      },
     },
   });
 
   return data;
 }
-export default function Home({searchParams}:{searchParams?:{filter?:string}}) {
-
-  
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: { filter?: string };
+}) {
   return (
     <div className="container mx-auto px-5 lg:px-10">
       <MapFilterItem />
-      <Suspense  key={searchParams?.filter} fallback={<SkeletonLoading/>}>
+      <Suspense key={searchParams?.filter} fallback={<SkeletonLoading />}>
         <ShowItems />
       </Suspense>
     </div>
@@ -47,29 +57,40 @@ async function ShowItems({
 }: {
   searchParams?: { filter?: string };
 }) {
-  const data = await getData({ searchParams: searchParams });
-return (
- <>
- {data.length === 0?(
-<Noitems/>
- ):(
-   <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
-    {data.map((item) => (
-      <ListingCard
-        key={item.id}
-        description={item.description as string}
-        imagePath={item.photo as string}
-        location={item.country as string}
-        price={item.price as number}
-      />
-    ))}
-  </div>
- )}
- </>
-);
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData({ searchParams: searchParams, userId: user?.id });
+  return (
+    <>
+      {data.length === 0 ? (
+        <Noitems
+          description="please check other category or create or own listing"
+          title="sorry no listing found 
+for ths category"
+        />
+      ) : (
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+          {data.map((item) => (
+            <ListingCard
+              key={item.id}
+              description={item.description as string}
+              imagePath={item.photo as string}
+              location={item.country as string}
+              price={item.price as number}
+              userId={user?.id}
+              FavoriteId={item.Favorite[0]?.id}
+              isInFavoritesList={item.Favorite.length > 0 ? true : false}
+              homeId={item.id}
+              pathName="/"
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
 
-function SkeletonLoading(){
+function SkeletonLoading() {
   return (
     <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
       <SkeletonCard />
